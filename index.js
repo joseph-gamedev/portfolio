c45012b3e60040f096ce43311fbc1529a835699c
joseph-gamedev <joseph3713@gmail.com>
2024-07-08 15:45:11 +0530

    Tutorial

--- index.js
+++
@@ -1,7 +0,0 @@
-document.addEventListener("DOMContentLoaded", function() {
-    // Example JavaScript to toggle a class on click
-    const introSection = document.getElementById("introduction");
-    introSection.addEventListener("click", function() {
-        this.classList.toggle("highlight");
-    });
-});
